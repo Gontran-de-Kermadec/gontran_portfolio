1be7ea35061b054bf980798002b99202bfd0e937
Gontran de Kermadec <gontrandekermadec@gmail.com>
2021-05-26 22:05:47 +0200

    animation terminée

--- js/index.js
+++ js/index.js
@@ -8,59 +8,45 @@ for(let i = 0; i < pieces.length; i++) {
     })
 }
 //section projets
-// document.querySelector(".main-container__projets").addEventListener('mouseover', (e)=> {
-//     const tl = gsap.timeline(); 
-//     tl.to(".projet__piece", 
-//     {
-//         top: "50%", 
-//         left: "50%",
-//         x: "-50%",
-//         y: "-50%",
-//         duration: .8,
-//         opacity: 1,
-//         scale: 1
-//     });
-//     tl.to(".main-container__projets .ten", 
-//     {
-//         scale: 3,
-//         borderRadius: 10,
-//         padding: 15
-//     });
-//     tl.to(".main-container__projets .ten", 
-//     {
-//         color: '#000',
-//     });
-// })
-// document.querySelector(".main-container__projets").addEventListener('mouseout', (e)=> {
-//     gsap.to(".main-container__projets .ten", 
-//     {
-//         color: '#fff',
-//     });
-//     gsap.to('.main-container__projets .ten', {
-//             borderRadius: 50,
-//             delay: .5
-//     })
-//     gsap.to(".projet__piece", 
-//     {   
-//         scale: 1,
-//         duration: .5,
-//         delay: .5
-//     });
-//     const pieces = document.querySelectorAll('.projet__piece');
-//     for(let i = 0; i < pieces.length; i++) {
-//         gsap.to(pieces[i] , {
-//         //tl.to(pieces[i], {
-//            top: Math.random() * (300 - (-300)) + (-300),
-//            left: Math.random() * (500 - (-500)) + (-500),
-//            color: '#fff',
-//            duration: .8,
-//            opacity: 0.5,
-//            scale: Math.random() * 2,
-//            delay: 0.8
-//         })
-//        console.log(Math.random() * (500 - (-500)) + (-500))
-//     }
-// })
+document.querySelector(".main-container__projets").addEventListener('mouseover', (e)=> {
+    const tl = gsap.timeline(); 
+    tl.to(".projet__piece", 
+    {
+        top: "50%", 
+        left: "50%",
+        x: "-50%",
+        y: "-50%",
+        duration: .8,
+        opacity: 1,
+        scale: 1
+    });
+    tl.to(".projet__piece", 
+    {
+        scale: 3,
+        duration: .8,
+    });
+})
+document.querySelector(".main-container__projets").addEventListener('mouseout', (e)=> {
+    gsap.to(".projet__piece", 
+    {   
+        scale: 1,
+        duration: .5,
+        delay: 1
+    });
+    const pieces = document.querySelectorAll('.projet__piece');
+    for(let i = 0; i < pieces.length; i++) {
+        gsap.to(pieces[i] , {
+        //tl.to(pieces[i], {
+           top: Math.random() * (300 - (-300)) + (-300),
+           left: Math.random() * (500 - (-500)) + (-500),
+           color: '#fff',
+           duration: .8,
+           opacity: 0.5,
+           scale: Math.random() * 2,
+           delay: 1.5
+        })
+    }
+})
 
 
 // section about
@@ -76,27 +62,14 @@ document.querySelector(".main-container__about").addEventListener('mouseover', (
         opacity: 1,
         scale: 1
     });
-    // tl.to(".main-container__about .ten", 
-    // {
-    //     scale: 3,
-    //     borderRadius: 10,
-    //     padding: 15
-    // });
-    // tl.to(".main-container__about .ten", 
-    // {
-    //     color: '#000',
-    // });
+    tl.to(".about__piece", 
+    {
+        scale: 3,
+        duration: .8,
+    });
 })
 
 document.querySelector(".main-container__about").addEventListener('mouseout', (e)=> {
-    // gsap.to(".main-container__about .ten", 
-    // {
-    //     color: '#fff',
-    // });
-    // gsap.to('.main-container__about .ten', {
-    //         borderRadius: 50,
-    //         delay: .5
-    // })
     gsap.to(".about__piece", 
     {   
         scale: 1,
@@ -115,7 +88,6 @@ document.querySelector(".main-container__about").addEventListener('mouseout', (e
            scale: Math.random() * 2,
            delay: 1.5
         })
-       console.log(Math.random() * (500 - (-500)) + (-500))
     }
 })
 
