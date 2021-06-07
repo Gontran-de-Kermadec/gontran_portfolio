1055b80e1d45d38a4b9503d352ef4a808b08c0dd
Gontran de Kermadec <gontrandekermadec@gmail.com>
2021-06-07 21:57:01 +0200

    mise en place transitions entre pages

--- js/index.js
+++ js/index.js
@@ -1,3 +1,104 @@
+function test () {
+    document.querySelector('.btn').addEventListener('click', () => {
+    //document.querySelector('button').addEventListener('click', () => {
+        document.querySelector('.try').innerHTML = "kikooo";
+        console.log('kickckkc');
+    })
+}
+test();
+
+
+
+
+function hoverAnimation () {
+    console.log('changement');
+    //section projets
+    if(document.querySelector(".main-container__projets")) {
+        document.querySelector(".main-container__projets").addEventListener('mouseover', (e)=> {
+            const tl = gsap.timeline(); 
+            tl.to(".projet__piece", 
+            {
+                top: "50%", 
+                left: "50%",
+                x: "-50%",
+                y: "-50%",
+                duration: .5,
+                opacity: 1,
+                scale: 1
+            });
+            tl.to(".projet__piece", 
+            {
+                scale: 3,
+                duration: .5,
+            });
+        })
+        document.querySelector(".main-container__projets").addEventListener('mouseout', (e)=> {
+            gsap.to(".projet__piece", 
+            {   
+                scale: 1,
+                duration: .5,
+                delay: .2
+            });
+            const pieces = document.querySelectorAll('.projet__piece');
+            for(let i = 0; i < pieces.length; i++) {
+                gsap.to(pieces[i] , {
+                //tl.to(pieces[i], {
+                   top: Math.random() * (300 - (-300)) + (-300),
+                   left: Math.random() * (500 - (-500)) + (-500),
+                   color: '#fff',
+                   duration: .8,
+                   opacity: 0.5,
+                   scale: Math.random() * 2,
+                   delay: .7
+                })
+            }
+        })
+    }
+    // section about
+    if(document.querySelector(".main-container__about")) {
+        document.querySelector(".main-container__about").addEventListener('mouseover', (e)=> {
+            const tl = gsap.timeline(); 
+            tl.to(".about__piece", 
+            {
+                top: "50%", 
+                left: "50%",
+                x: "-50%",
+                y: "-50%",
+                duration: .5,
+                opacity: 1,
+                scale: 1
+            });
+            tl.to(".about__piece", 
+            {
+                scale: 3,
+                duration: .5,
+            });
+        })
+
+        document.querySelector(".main-container__about").addEventListener('mouseout', (e)=> {
+            gsap.to(".about__piece", 
+            {   
+                scale: 1,
+                duration: .5,
+                delay: .2
+            });
+            const pieces = document.querySelectorAll('.about__piece');
+            for(let i = 0; i < pieces.length; i++) {
+                gsap.to(pieces[i] , {
+                //tl.to(pieces[i], {
+                top: Math.random() * (300 - (-300)) + (-300),
+                left: Math.random() * (500 - (-500)) + (-500),
+                color: '#fff',
+                duration: .8,
+                opacity: 0.5,
+                scale: Math.random() * 2,
+                delay: .7
+                })
+            }
+        })
+    }
+}
+hoverAnimation();
 const pieces = document.querySelectorAll('.piece');
 for(let i = 0; i < pieces.length; i++) {
     pieces[i].addEventListener('mouseover', (e)=> {
@@ -7,88 +108,102 @@ for(let i = 0; i < pieces.length; i++) {
         e.stopPropagation();
     })
 }
+
+const options = {
+    cache: false,
+    animateHistoryBrowsing: true
+  };
+const swup = new Swup(options);
+swup.on('contentReplaced', hoverAnimation)
+swup.on('contentReplaced', test)
+//swup.on('willReplaceContent', hoverAnimation);
 //section projets
-document.querySelector(".main-container__projets").addEventListener('mouseover', (e)=> {
-    const tl = gsap.timeline(); 
-    tl.to(".projet__piece", 
-    {
-        top: "50%", 
-        left: "50%",
-        x: "-50%",
-        y: "-50%",
-        duration: .8,
-        opacity: 1,
-        scale: 1
-    });
-    tl.to(".projet__piece", 
-    {
-        scale: 3,
-        duration: .8,
-    });
-})
-document.querySelector(".main-container__projets").addEventListener('mouseout', (e)=> {
-    gsap.to(".projet__piece", 
-    {   
-        scale: 1,
-        duration: .5,
-        delay: 1
-    });
-    const pieces = document.querySelectorAll('.projet__piece');
-    for(let i = 0; i < pieces.length; i++) {
-        gsap.to(pieces[i] , {
-        //tl.to(pieces[i], {
-           top: Math.random() * (300 - (-300)) + (-300),
-           left: Math.random() * (500 - (-500)) + (-500),
-           color: '#fff',
-           duration: .8,
-           opacity: 0.5,
-           scale: Math.random() * 2,
-           delay: 1.5
-        })
-    }
-})
+// document.querySelector(".main-container__projets").addEventListener('mouseover', (e)=> {
+//     const tl = gsap.timeline(); 
+//     tl.to(".projet__piece", 
+//     {
+//         top: "50%", 
+//         left: "50%",
+//         x: "-50%",
+//         y: "-50%",
+//         duration: .5,
+//         opacity: 1,
+//         scale: 1
+//     });
+//     tl.to(".projet__piece", 
+//     {
+//         scale: 3,
+//         duration: .5,
+//     });
+// })
+// document.querySelector(".main-container__projets").addEventListener('mouseout', (e)=> {
+//     const tl = gsap.timeline();
+//     gsap.to(".projet__piece", 
+//     //tl.to(".projet__piece", 
+//     {   
+//         scale: 1,
+//         duration: .5,
+//         delay: 0.5
+//     });
+//     const pieces = document.querySelectorAll('.projet__piece');
+//     for(let i = 0; i < pieces.length; i++) {
+//         gsap.to(pieces[i] , 
+//         //tl.to(pieces[i], 
+//         {
+//            top: Math.random() * (300 - (-300)) + (-300),
+//            left: Math.random() * (500 - (-500)) + (-500),
+//            color: '#fff',
+//            duration: .8,
+//            opacity: 0.5,
+//            scale: Math.random() * 2,
+//            delay: 0.8
+//         })
+//     }
+// })
 
 
 // section about
-document.querySelector(".main-container__about").addEventListener('mouseover', (e)=> {
-    const tl = gsap.timeline(); 
-    tl.to(".about__piece", 
-    {
-        top: "50%", 
-        left: "50%",
-        x: "-50%",
-        y: "-50%",
-        duration: .8,
-        opacity: 1,
-        scale: 1
-    });
-    tl.to(".about__piece", 
-    {
-        scale: 3,
-        duration: .8,
-    });
-})
+// document.querySelector(".main-container__about").addEventListener('mouseover', (e)=> {
+//     const tl = gsap.timeline(); 
+//     tl.to(".about__piece", 
+//     {
+//         top: "50%", 
+//         left: "50%",
+//         x: "-50%",
+//         y: "-50%",
+//         duration: .8,
+//         opacity: 1,
+//         scale: 1
+//     });
+//     tl.to(".about__piece", 
+//     {
+//         scale: 3,
+//         duration: .8,
+//     });
+// })
 
-document.querySelector(".main-container__about").addEventListener('mouseout', (e)=> {
-    gsap.to(".about__piece", 
-    {   
-        scale: 1,
-        duration: .5,
-        delay: 1
-    });
-    const pieces = document.querySelectorAll('.about__piece');
-    for(let i = 0; i < pieces.length; i++) {
-        gsap.to(pieces[i] , {
-        //tl.to(pieces[i], {
-           top: Math.random() * (300 - (-300)) + (-300),
-           left: Math.random() * (500 - (-500)) + (-500),
-           color: '#fff',
-           duration: .8,
-           opacity: 0.5,
-           scale: Math.random() * 2,
-           delay: 1.5
-        })
-    }
-})
+// document.querySelector(".main-container__about").addEventListener('mouseout', (e)=> {
+//     gsap.to(".about__piece", 
+//     {   
+//         scale: 1,
+//         duration: .5,
+//         delay: 1
+//     });
+//     const pieces = document.querySelectorAll('.about__piece');
+//     for(let i = 0; i < pieces.length; i++) {
+//         gsap.to(pieces[i] , {
+//         //tl.to(pieces[i], {
+//            top: Math.random() * (300 - (-300)) + (-300),
+//            left: Math.random() * (500 - (-500)) + (-500),
+//            color: '#fff',
+//            duration: .8,
+//            opacity: 0.5,
+//            scale: Math.random() * 2,
+//            delay: 1.5
+//         })
+//     }
+// })
+
+
 
 
--- js/three.js
+++ js/three.js
@@ -1,35 +1,74 @@
-import * as THREE from 'https://cdn.skypack.dev/three@0.128.0';
-import {OrbitControls} from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/controls/OrbitControls.js';
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+//import * as THREE from 'https://cdn.skypack.dev/three@0.128.0';
+//import {OrbitControls} from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/controls/OrbitControls.js';
+// const scene = new THREE.Scene();
+// const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer({
-    antialias: true,
-    alpha: true
-});
-renderer.setSize(window.innerWidth, innerHeight);
+// const renderer = new THREE.WebGLRenderer({
+//     antialias: true,
+//     alpha: true
+// });
+// renderer.setSize(window.innerWidth, innerHeight);
 //document.body.appendChild(renderer.domElement);
+
+
+
+
 //document.querySelector(".threejs").appendChild(renderer.domElement);
-document.querySelector(".threejs").appendChild(renderer.domElement);
 //const controls = new OrbitControls( camera, renderer.domElement );
 
-const geometry = new THREE.BoxGeometry(3,1,0.3);
+//const geometry = new THREE.BoxGeometry(3,1,0.3);
 //const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const mat = new THREE.MeshNormalMaterial();
-const cube = new THREE.Mesh( geometry, mat );
+//const mat = new THREE.MeshNormalMaterial();
+//const cube = new THREE.Mesh( geometry, mat );
 //const cube = new THREE.Mesh( geometry, material );
 //scene.add(new THREE.AxisHelpers());
-scene.add( cube );
+//scene.add( cube );
 
-camera.position.z = 5;
+//camera.position.z = 5;
 //camera.position.x = 3;
-camera.position.y = 0;
+//camera.position.y = 0;
 
-function animate() {
-	requestAnimationFrame( animate );
-    cube.rotation.x += 0.01;
-    //cube.rotation.y += 0.01;
-    //controls.update();
-	renderer.render( scene, camera );
+// function animate() {
+// 	requestAnimationFrame( animate );
+//     cube.rotation.x += 0.01;
+//     //cube.rotation.y += 0.01;
+//     //controls.update();
+// 	renderer.render( scene, camera );
+// }
+// animate();
+
+function threeAnimation () {
+    if(document.querySelector(".threejs")) {
+        console.log('detection change');
+        const scene = new THREE.Scene();
+        const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+        const renderer = new THREE.WebGLRenderer({
+            antialias: true,
+            alpha: true
+        });
+        renderer.setSize(window.innerWidth, innerHeight);
+        document.querySelector(".threejs").appendChild(renderer.domElement);
+        const geometry = new THREE.BoxGeometry(3,1,0.3);
+        const mat = new THREE.MeshNormalMaterial();
+        const cube = new THREE.Mesh( geometry, mat );
+        scene.add( cube );
+
+        camera.position.z = 5;
+        camera.position.y = 0;
+
+        function animate() {
+            requestAnimationFrame( animate );
+            cube.rotation.x += 0.01;
+            renderer.render( scene, camera );
+        }
+        animate();
+    }
 }
-animate();
+threeAnimation();
+
+const options = {
+    cache: false,
+    animateHistoryBrowsing: true
+  };
+const swup = new Swup(options);
+swup.on('contentReplaced', threeAnimation)
